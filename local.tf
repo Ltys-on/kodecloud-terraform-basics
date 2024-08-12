633908fd1eae9d885311554d662ef9679d6e46bd
Liam Tyson <liam.tyson@ncorporate.co.uk>
2024-08-12 15:53:20 +0100

    More terraform configuration

--- local.tf
+++ local.tf
@@ -1,4 +1,11 @@
 resource "local_file" "pet" {
-  filename = "/Users/ltys/repos/kodecloud-terraform-basics/pets.txt"
-  content  = "We love pets!"
+  filename        = "/Users/ltys/repos/kodecloud-terraform-basics/pets.txt"
+  content         = "We love pets!"
+  file_permission = "0700"
+}
+
+resource "random_pet" "my-pet" {
+  prefix    = "Mrs"
+  separator = "."
+  length    = "1"
 }
